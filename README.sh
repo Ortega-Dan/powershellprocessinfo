8337935a3fa2d43144c2a164fe2df2189e3a913c
Dan Ortega <ortega.dan2010@gmail.com>
2020-01-31 12:59:19 -0500

    Create README.sh

---
+++ README.sh
@@ -0,0 +1,17 @@
+# powershellprocessinfo
+In case it helps
+
+
+## Get information of a process
+$maprocess = Get-CimInstance -Class Win32_Process -Filter "Name = 'firefox.exe'"
+
+# Get the command of a process
+$maprocess[0].ParentProcessId
+
+$maprocess[0].CommandLine
+
+## You are able to get some aditional stuff by doing
+$maprocess[0].
+
+# ... and BEFORE HITTING ENTER/ you hit Ctrl + Space ... and it will 
+# offer you all the options that object has
